6ab6f0ee976a534d555bd53696c08f805c79dcdb
leo <lwoschke@redcode.org>
2026-03-11 10:01:58 +0100

    clean up base files

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,14 +3,16 @@
 namespace Redcodede\Captcha;
 
 use Statamic\Providers\AddonServiceProvider;
+use Redcodede\Captcha\Tags\RefreshCaptcha;
+use Redcodede\Captcha\Tags\CaptchaImage;
 
 class ServiceProvider extends AddonServiceProvider
 {
     public $captcha;
 
     protected $tags = [
-        \Redcodede\Captcha\Tags\RefreshCaptcha::class,
-        \Redcodede\Captcha\Tags\CaptchaImage::class,
+        RefreshCaptcha::class,
+        CaptchaImage::class,
     ];
     public function bootAddon()
     {
